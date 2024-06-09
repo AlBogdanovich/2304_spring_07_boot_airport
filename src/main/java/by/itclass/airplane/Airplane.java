fd90fbfe7ce8978b5fc4588f7010ef5d0970b6f0
AlBogdanovich <alenuschkabogdanovich@gmail.com>
2024-06-09 14:33:12 +0300

    Airport last

---
+++ src/main/java/by/itclass/airplane/Airplane.java
@@ -0,0 +1,27 @@
+package by.itclass.airplane;
+
+import by.itclass.passenger.Passenger;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import java.util.List;
+
+@Entity
+@Table(name = "airplane")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Data
+public class Airplane {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NonNull
+    private String model;
+    @NonNull
+    private int places;
+    @OneToMany(mappedBy = "airplane", fetch = FetchType.EAGER)
+    private List<Passenger> passengers;
+
+}
