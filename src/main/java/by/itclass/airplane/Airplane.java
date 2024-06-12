7757ee13ed9af7d7226bab6c1ecfed6a52e17107
AlBogdanovich <alenuschkabogdanovich@gmail.com>
2024-06-12 23:11:41 +0300

    Airport last

--- src/main/java/by/itclass/airplane/Airplane.java
+++ src/main/java/by/itclass/airplane/Airplane.java
@@ -1,5 +1,6 @@
 package by.itclass.airplane;
 
+import by.itclass.flight.Flight;
 import by.itclass.passenger.Passenger;
 import jakarta.persistence.*;
 import lombok.*;
@@ -12,16 +13,15 @@ import java.util.List;
 @NoArgsConstructor
 @RequiredArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 public class Airplane {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @NonNull
-    private String model;
-    @NonNull
-    private int places;
+    @NonNull private String model;
+    @NonNull private int places;
     @OneToMany(mappedBy = "airplane", fetch = FetchType.EAGER)
-    private List<Passenger> passengers;
+    private List<Flight> flights;
 
 }
--- src/main/java/by/itclass/airplane/AirplaneController.java
+++ src/main/java/by/itclass/airplane/AirplaneController.java
@@ -1,9 +1,12 @@
 package by.itclass.airplane;
 
+import jakarta.servlet.http.PushBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -15,19 +18,30 @@ public class AirplaneController {
         this.repository = repository;
     }
 
-    @GetMapping
-    public String getAllAirplanes(Model model) {
-        var airplanes = repository.findAll();
-        model.addAttribute("airplanes", airplanes);
-        return "index";
-    }
+   @GetMapping
+   public String main() {
+       return "airport";
+   }
 
-    @GetMapping("/view")
-    public String viewPlane(
-            @RequestParam(name= "plane") int airplaneId,
-            Model model) {
-        var airplane = repository.findById(airplaneId).get();
-        model.addAttribute("airplane", airplane);
-        return "airplane";
-    }
+   @GetMapping("/allPlanes")
+    public String getAllPlanes(Model model) {
+        var planes = repository.findAll();
+        model.addAttribute("planes", planes);
+        return "airplanes";
+   }
+
+   @PostMapping("/addPlane")
+    public String addPlane(
+            @RequestParam(name = "model") String model,
+            @RequestParam(name = "places") int places) {
+        repository.save(new Airplane(model, places));
+        return "redirect:/allPlanes";
+   }
+
+   @GetMapping("/delPlane/{id}")
+    public String delPlane(
+            @PathVariable(name = "id") int planeId) {
+        repository.deleteById(planeId);
+        return "redirect:/allPlanes";
+   }
 }
